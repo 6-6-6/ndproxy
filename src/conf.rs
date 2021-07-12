9c50760811492961bcf44e016fb808eac2da4ffb
Zhang Zongyu <zongyu@novazy.net>
2021-07-12 12:54:51 +0800

    format src/conf.rs
    Signed-off-by: Zhang Zongyu <zongyu@novazy.net>

--- src/conf.rs
+++ src/conf.rs
@@ -1,8 +1,7 @@
-use std::collections::HashMap;
 use ipnet::Ipv6Net;
+use std::collections::HashMap;
 
-#[derive(getset::Getters)]
-#[derive(Debug, std::cmp::PartialEq, Clone)]
+#[derive(getset::Getters, Debug, std::cmp::PartialEq, Clone)]
 pub struct NDConfig {
     #[get = "pub with_prefix"]
     proxy_type: u8,
@@ -20,11 +19,18 @@ pub struct NDConfig {
 
 const PROXY_FORWARD: &str = "forward";
 
-impl NDConfig {    
-
+impl NDConfig {
     pub fn new(config_table: HashMap<String, config::Value>) -> Self {
-        // magic words
-        let proxy_type_string = config_table.get("type").unwrap().clone().into_str().unwrap();
+        /*
+         * there must be a field for "type",
+         * so that we can decide the way to proxy Neighbor Discoverys
+         */
+        let proxy_type_string = config_table
+            .get("type")
+            .unwrap()
+            .clone()
+            .into_str()
+            .unwrap();
 
         let proxy_type: u8;
         if proxy_type_string == PROXY_FORWARD {
@@ -33,50 +39,86 @@ impl NDConfig {
             proxy_type = 0;
         }
 
-        // TODO: check the length of the netmask
-        let proxied_pfx: Ipv6Net = config_table.get("proxied_prefix").unwrap().clone()
-                                                .into_str().unwrap()
-                                                .parse().unwrap();
+        /*
+         * there must be a field for "proxied_prefix",
+         * to inform us which prefix is going to be proxied
+         */
+        let proxied_pfx: Ipv6Net = config_table
+            .get("proxied_prefix")
+            .unwrap()
+            .clone()
+            .into_str()
+            .unwrap()
+            .parse()
+            .unwrap();
+        // TODO: is it necessary to check the prefix length and address type?
 
+        /*
+         * get the interfaces whose Neighbor Solicitations are proxied by me
+         * if it is not specified, I will listen on all of the interfaces
+         */
         let proxied_ifaces = match config_table.get("proxied_ifaces") {
             Some(v) => {
                 let mut ifaces = Vec::new();
                 match v.clone().into_array() {
-                    Ok(if_vec) => for iface in if_vec.iter() {
-                        ifaces.push(iface.clone().into_str().unwrap());
-                    },
-                    Err(_) =>  ifaces.push(v.clone().into_str().unwrap()),
+                    Ok(if_vec) => {
+                        for iface in if_vec.iter() {
+                            ifaces.push(iface.clone().into_str().unwrap());
+                        }
+                    }
+                    Err(_) => ifaces.push(v.clone().into_str().unwrap()),
                 }
                 ifaces
-            },
+            }
             None => vec![String::from("*")],
         };
 
+        /*
+         * get the interfaces whose Neighbor Advertisements are proxied by me
+         * if it is not specified, I will listen on all of the interfaces
+         */
         let forwarded_ifaces = match config_table.get("forwarded_ifaces") {
             Some(v) => {
                 let mut ifaces = Vec::new();
                 match v.clone().into_array() {
-                    Ok(if_vec) => for iface in if_vec.iter() {
-                        ifaces.push(iface.clone().into_str().unwrap());
-                    },
-                    Err(_) =>  ifaces.push(v.clone().into_str().unwrap()),
+                    Ok(if_vec) => {
+                        for iface in if_vec.iter() {
+                            ifaces.push(iface.clone().into_str().unwrap());
+                        }
+                    }
+                    Err(_) => ifaces.push(v.clone().into_str().unwrap()),
                 }
                 ifaces
-            },
+            }
             None => vec![String::from("*")],
         };
 
+        /*
+         * extra recipe: rewrite the address
+         * let's say your network relys on Network Prefix Translation.
+         *
+         * For instance:
+         *   2001:db8:1::/64                  2001:db8:ffff::/64
+         *         ↑                                  ↑
+         *        ISP1                               ISP2
+         *         |-------- fec1:2:3:4::/64 ---------|
+         *
+         * Maybe it is suitble for you to specify the Rewrite field like:
+         * ```
+         * rewrite = "fec1:2:3:4::/64"
+         * ```
+         */
         let dst_pfx: Ipv6Net;
         let rewrite: bool;
         match config_table.get("rewrite") {
             Some(v) => {
                 dst_pfx = v.clone().into_str().unwrap().parse().unwrap();
                 rewrite = true;
-            },
+            }
             None => {
                 dst_pfx = proxied_pfx;
                 rewrite = false;
-            },
+            }
         }
 
         NDConfig {
@@ -90,7 +132,11 @@ impl NDConfig {
     }
 }
 
-
+/*
+ * parse the toml configuration file, returns a vector of NDConfig
+ *
+ * Note that there MUST be a master section called "ndp"
+ */
 pub fn parse_config(cfile: &str) -> Vec<NDConfig> {
     let mut ret = Vec::new();
     let mut myconfig = config::Config::new();
@@ -100,7 +146,7 @@ pub fn parse_config(cfile: &str) -> Vec<NDConfig> {
     // magic word: ndp
     for value in myconfig.get_table("ndp").unwrap().values() {
         ret.push(NDConfig::new(value.clone().into_table().unwrap()));
-    };
+    }
     ret
 }
 
